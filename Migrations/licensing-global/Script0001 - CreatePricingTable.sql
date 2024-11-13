5462891d7efd1f4b798e6ded8c5e89df3ae02b26
Matt Varley <matthew.varley@rfsmart.com>
2024-11-13 11:13:28 +0000

    Update migrations

--- Migrations/licensing-global/Script0001 - CreatePricingTable.sql
+++ Migrations/licensing-global/Script0001 - CreatePricingTable.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS licensing.pricing
 (
     row_id                  int  NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    license                 text COLLATE pg_catalog."default" NOT NULL,
+    feature_name            text COLLATE pg_catalog."default" NOT NULL,
     price_per_user          numeric(5,2) NOT NULL DEFAULT 0,
+    price_on_demand         numeric(5,2) NOT NULL DEFAULT 0,
     enforced_from           timestamp with time zone NOT NULL,
+    enforced_until          timestamp with time zone NOT NULL,
     created                 timestamp with time zone NOT NULL,
     created_by              text COLLATE pg_catalog."default" NOT NULL,
     last_modified           timestamp with time zone NOT NULL,
@@ -13,5 +15,5 @@ CREATE TABLE IF NOT EXISTS licensing.pricing
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS accounts.users
+ALTER TABLE IF EXISTS licensing.pricing
     OWNER to $owner$;
--- Migrations/licensing-tenant/Script0001 - CreateLicensingTables.sql
+++ Migrations/licensing-tenant/Script0001 - CreateLicensingTables.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS $schema_name$.licensing_issued
     created_by              text COLLATE pg_catalog."default" NOT NULL,
     last_modified           timestamp with time zone NOT NULL,
     last_modified_by        text COLLATE pg_catalog."default" NOT NULL,    
-    license_name            text COLLATE pg_catalog."default" NOT NULL,
+    feature_name            text COLLATE pg_catalog."default" NOT NULL,
     enabled_time            timestamp with time zone NOT NULL,
     disabled_time           timestamp with time zone NOT NULL,
     users                   numeric NOT NULL DEFAULT 0
@@ -13,17 +13,15 @@ CREATE TABLE IF NOT EXISTS $schema_name$.licensing_issued
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS $schema_name$.licensing_tracking
+ALTER TABLE IF EXISTS $schema_name$.licensing_issued
     OWNER to $owner$;
 
 CREATE TABLE IF NOT EXISTS $schema_name$.licensing_tracking
 (
     row_id                  int  NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    created                 timestamp with time zone NOT NULL,
-    created_by              text COLLATE pg_catalog."default" NOT NULL,
-    license_name            text COLLATE pg_catalog."default" NOT NULL,
-    event_name              text COLLATE pg_catalog."default" NOT NULL,
-    role_name               text COLLATE pg_catalog."default" NOT NULL
+    feature_name            text COLLATE pg_catalog."default" NOT NULL,
+    users                   text[] NOT NULL DEFAULT array[]::text[],
+    created                 timestamp with time zone NOT NULL
 )
 
 TABLESPACE pg_default;
