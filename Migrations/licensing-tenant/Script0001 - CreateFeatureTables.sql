288c7e83baa891ae6882ae850eb9ad722a58bacf
Matt Varley <matthew.varley@rfsmart.com>
2024-11-18 17:11:05 +0000

    Add indexes

--- Migrations/licensing-tenant/Script0001 - CreateFeatureTables.sql
+++ Migrations/licensing-tenant/Script0001 - CreateFeatureTables.sql
@@ -16,6 +16,9 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS $schema_name$.feature_issued
     OWNER to $owner$;
 
+CREATE INDEX feature_issued_feature_name_idx ON $schema_name$.feature_issued 
+ USING btree (feature_name, created DESC);
+
 CREATE TABLE IF NOT EXISTS $schema_name$.feature_tracking
 (
     row_id                  int  NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -28,3 +31,10 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS $schema_name$.feature_tracking
     OWNER to $owner$;
+
+CREATE INDEX feature_tracking_feature_name_idx ON $schema_name$.feature_tracking 
+ USING btree (feature_name, created DESC);
+
+CREATE INDEX feature_tracking_created_idx ON $schema_name$.feature_tracking 
+ USING BRIN (created)
+ WITH (pages_per_range = 32);
