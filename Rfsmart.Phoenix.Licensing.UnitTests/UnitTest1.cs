c46e64d5b635ce2b0ff49f699006e5658de909bc
Matt Varley <matthew.varley@rfsmart.com>
2024-11-18 17:11:13 +0000

    Add data generator

--- Rfsmart.Phoenix.Licensing.UnitTests/UnitTest1.cs
+++ Rfsmart.Phoenix.Licensing.UnitTests/UnitTest1.cs
@@ -1,7 +1,15 @@
+using Rfsmart.Phoenix.Licensing.Helpers;
+using Rfsmart.Phoenix.Licensing.Models;
+using System.Globalization;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace Rfsmart.Phoenix.Licensing.UnitTests;
 
 public class Tests
 {
+    
     [SetUp]
     public void Setup()
     {
@@ -10,6 +18,93 @@ public class Tests
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        var trackedRecords = DataGenerator.Generate(1000, 30);
+
+        //var result = JsonSerializer.Serialize(trackedRecords.OrderBy(x => x.Created));
+
+        var features = trackedRecords.Select(x => x.FeatureName).Distinct();
+        WriteCsvForExcelCharts(trackedRecords.OrderBy(x => x.Created), "C:\\Dev\\testdata.csv", features.ToArray());
+    }
+
+    public static void WriteCsv<T>(IOrderedEnumerable<T> items, string path)
+    {
+        Type itemType = typeof(T);
+        var props = itemType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                            .Where(pi => pi.Name != "UserCount")
+                            .ToArray();
+
+        using (var writer = new StreamWriter(path))
+        {
+            // Writing header (property names)
+            writer.WriteLine(string.Join(",", props.Select(p => p.Name)));
+
+            // Writing data
+            foreach (var item in items)
+            {
+                writer.WriteLine(string.Join(",", props.Select(p => GetValue(p, item!))));
+            }
+        }
+    }
+
+    private static string GetValue(PropertyInfo propertyInfo, object obj)
+    {
+        if (propertyInfo.PropertyType.IsArray)
+        {
+            var users = propertyInfo.GetValue(obj) as IEnumerable<string>;
+
+            return $"\"{{{string.Join(',', users!)}}}\"";
+        }
+
+        if (propertyInfo.PropertyType == typeof(DateTime))
+        {
+            var val = propertyInfo.GetValue(obj, null) as DateTime?;
+
+            return val!.Value.ToString("o", CultureInfo.InvariantCulture)!;
+        }
+
+        var value = propertyInfo.GetValue(obj, null);
+        return value?.ToString()!.Replace(",", ";") ?? "";  // Escape commas in data
+    }
+
+    public static void WriteCsvForExcelCharts(IOrderedEnumerable<FeatureTrackingRecord> items, string path, string[] headers)
+    {
+        using (var writer = new StreamWriter(path))
+        {
+            var columns = new List<string>(["", ..headers]);
+
+            writer.WriteLine(string.Join(",", columns));
+
+            // empty,Receiving,Advanced Receiving,Picking
+            var processed = new List<FeatureTrackingRecord>();
+
+            // Writing data
+            foreach (var item in items)
+            {
+                string[] columnValues = columns.Select((col, index) =>
+                {
+                    if (index == 0)
+                    {
+                        return item.Created.ToString("o", CultureInfo.InvariantCulture);
+                    }
+
+                    if (col == item.FeatureName)
+                    {
+                        return item.UserCount.ToString();
+                    }
+
+                    var last = processed.LastOrDefault(x => x.FeatureName == item.FeatureName);
+
+                    if (last != null)
+                    {
+                        return last.UserCount.ToString();
+                    }
+
+                    return "0";
+                }).ToArray();
+
+                processed.Add(item);
+                writer.WriteLine(string.Join(",", columnValues));
+            }
+        }
     }
 }
---
+++ Rfsmart.Phoenix.Licensing/Helpers/DataGenerator.cs
@@ -0,0 +1,100 @@
+﻿using Rfsmart.Phoenix.Licensing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rfsmart.Phoenix.Licensing.Helpers
+{
+    public static class DataGenerator
+    {
+        public static List<FeatureTrackingRecord> Generate(int totalRecords = 5000, int elapsedDays = 30)
+        {
+            var startTime = DateTime.UtcNow.AddDays(elapsedDays * -1);
+            var endTime = DateTime.UtcNow;
+            var timeSpan = endTime - startTime;
+
+            string[] actions = ["Add", "Remove"];
+            string[] allUsers = ["Bob", "Gary", "Jeremy", "Steve", "Jenny", "Greg", "Heather", "Emma", "Kris", "Sharon", "Kristan", "Katie", "Vicky"];
+            string[] features = ["Receiving", "Advanced Receiving", "Picking"];
+
+            var receivingStartRecord = new FeatureTrackingRecord
+            {
+                FeatureName = "Receiving",
+                Users = [],
+                Created = startTime,
+            };
+
+            var advancedReceivingStartRecord = new FeatureTrackingRecord
+            {
+                FeatureName = "Advanced Receiving",
+                Users = [],
+                Created = startTime,
+            };
+
+            var pickingStartRecord = new FeatureTrackingRecord
+            {
+                FeatureName = "Picking",
+                Users = [],
+                Created = startTime,
+            };
+
+            var trackedRecords = new List<FeatureTrackingRecord>([receivingStartRecord, advancedReceivingStartRecord, pickingStartRecord]);
+
+            var timeStamps = Enumerable.Range(0, totalRecords).Select(x =>
+            {
+                var randomDate = new Random();
+
+                TimeSpan newSpan = new TimeSpan(0, randomDate.Next(0, (int)timeSpan.TotalMinutes), 0);
+                return startTime + newSpan;
+            }).OrderBy(x => x).ToList();
+
+            foreach (var time in timeStamps)
+            {
+                var randomAction = actions[new Random().Next(0, actions.Length)];
+                var randomFeature = features[new Random().Next(0, features.Length)];
+
+                var lastRecord = trackedRecords.Last(x => x.FeatureName == randomFeature);
+                var unassignedUsers = allUsers.Where(x => !lastRecord.Users.Contains(x)).ToArray();
+
+                if (unassignedUsers.Length == 0)
+                {
+                    randomAction = "Remove";
+                }
+
+                if (lastRecord.UserCount == 0 && randomAction == "Remove")
+                {
+                    randomAction = "Add";
+                }
+
+                string[] newUsers = [];
+
+                switch (randomAction)
+                {
+                    case "Add":
+                        var randomUser = unassignedUsers[new Random().Next(0, unassignedUsers.Length)];
+                        newUsers = [.. lastRecord.Users, randomUser];
+                        break;
+                    case "Remove":
+                        var ind = new Random().Next(0, lastRecord.UserCount - 1);
+                        newUsers = lastRecord.Users.Where((x, i) => i >= ind).ToArray();
+                        break;
+                    default:
+                        break;
+                }
+
+                var newRecord = new FeatureTrackingRecord
+                {
+                    Users = newUsers,
+                    FeatureName = randomFeature,
+                    Created = time,
+                };
+
+                trackedRecords.Add(newRecord);
+            }
+
+            return trackedRecords;
+        }
+    }
+}
